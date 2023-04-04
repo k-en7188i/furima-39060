d49e25c3a5e99e5b1a7d3196a75fa3a3bce46594
k-en7188i <127103189+k-en7188i@users.noreply.github.com>
2023-04-04 18:08:49 +0900

    マイグレーションファイルの削除・再設定、ログイン・新規登録画面への画面遷移設定

--- db/migrate/20230404053621_devise_create_users.rb
+++ db/migrate/20230404053621_devise_create_users.rb
@@ -4,8 +4,16 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :email,              null: false, default: ""
+      t.string :email,              null: false, unique: true, default: ""
       t.string :encrypted_password, null: false, default: ""
+      t.string :nickname,		        null: false
+      t.string :email,		          null: false, unique: true
+      t.string :encrypted_password,	null: false
+      t.string :first_name,		      null: false
+      t.string :last_name,		      null: false
+      t.string :first_name_kana,		null: false
+      t.string :last_name_kana,		  null: false
+      t.string :birth_date,	        null: false
 
       ## Recoverable
       t.string   :reset_password_token
@@ -36,8 +44,8 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       t.timestamps null: false
     end
 
-    add_index :users, :email,                unique: true
-    add_index :users, :reset_password_token, unique: true
+    add_index :users, :email,                  unique: true
+    add_index :users, :reset_password_token,   unique: true
     # add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
   end
