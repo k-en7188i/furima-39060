88f88e5f3a4abca3c34d01f34c1fa15bf48b7ef1
k-en7188i <127103189+k-en7188i@users.noreply.github.com>
2023-04-07 15:34:31 +0900

    プルダウンの作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,29 +1,31 @@
 class ItemsController < ApplicationController
+before_action :authenticate_user!, only: [:new, :create]
+
+
+
   def index
     @items = Item.all
     @items = Item.order("created_at DESC")
   end
 
   def new
-    @items = Item.new
+    @item = Item.new
   end
 
-  # def create
-  #   @item =Item.new(item_params)
-  #   if @item.save
-  #     redirect_to root_path
-  #   else
-  #     render :new
-  #   end
-  # end
+  def create
+    @item =Item.new(item_params)
+    if @item.save
+      binding.pry
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
 
   private
 
-    def message_params
-      params.require(:item).permit(:image).merge(user_id: current_user.id)
+    def item_params
+      params.require(:item).permit(:name, :description, :category_id, :condition_id, :shipping_fee_id, :prefecture_id, :shipping_day_id, :price, :image).merge(user_id: current_user.id)
     end
 
-    def item_params
-      params.require(:item).permit(:prefecture_id)
-    end
 end
