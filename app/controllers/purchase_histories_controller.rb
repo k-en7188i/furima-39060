7eecdcb91099b1c1cdb1b89600cedef78993027d
k-en7188i <127103189+k-en7188i@users.noreply.github.com>
2023-04-13 17:06:18 +0900

    購入画面への遷移制限設定

--- app/controllers/purchase_histories_controller.rb
+++ app/controllers/purchase_histories_controller.rb
@@ -1,13 +1,18 @@
 class PurchaseHistoriesController < ApplicationController
-  before_action :item_params,  only: [:index, :create]
-  # before_action :set_items,  only: [:index]
+  before_action :authenticate_user!
+  before_action :item_params,  only: [:index, :create, :new]
+  before_action :set_items,  only: [:index, :create, :new]
+  before_action :users_checking, only: [:index, :create, :new]  
+  before_action :items_checking, only: [:index, :create, :new]  
   
+
   def index
-    # @items = Item.all.order(created_at: :desc)
-    @purchase_shipping = PurchaseShipping.new
+    # @item = Item.all.order(created_at: :desc)
+    @purchase_shipping = PurchaseShipping.new 
   end
   
   def new
+    # @purchase_shipping = PurchaseShipping.new
   end
 
 
@@ -15,9 +20,10 @@ class PurchaseHistoriesController < ApplicationController
     @purchase_shipping = PurchaseShipping.new(purchase_history_params)
     if @purchase_shipping.valid? 
       @purchase_shipping.save
+      
       redirect_to root_path
     else
-      # set_items
+      # binding.pry
       render :index
     end
   end
@@ -29,17 +35,24 @@ class PurchaseHistoriesController < ApplicationController
     @item = Item.find(params[:item_id])
   end
 
-  # def set_items
-  #   @items = Item.all.order(created_at: :desc)
-  #   @purchase_shipping = PurchaseShipping.new
-  # end
-
-  def donation_params
-    params.permit(:price).merge(user_id: current_user.id)
+  def set_items
+    @items = Item.all.order(created_at: :desc)
+    # @purchase_shipping = PurchaseShipping.new
   end
 
   def purchase_history_params
+  # 理想系 # params.require(:purchase_shipping).permit(:postal_code, :prefecture_id, :city, :address, :building, :phone_number).merge(user_id: current_user.id, item_id: @item.id)
     params.permit(:postal_code, :prefecture_id, :city, :address, :building, :phone_number).merge(user_id: current_user.id, item_id: @item.id)
   end
   
+  def users_checking
+    return unless current_user.id == @item.user_id
+    redirect_to root_path
+  end
+
+  def items_checking
+    return unless @item.purchase_history.presence
+    redirect_to root_path 
+  end
+
 end
