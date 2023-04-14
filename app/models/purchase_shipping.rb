66a0fa1932666fca81f61c428dcbefb886eac3bd
k-en7188i <127103189+k-en7188i@users.noreply.github.com>
2023-04-14 16:59:42 +0900

    不要な記述の削除１

--- app/models/purchase_shipping.rb
+++ app/models/purchase_shipping.rb
@@ -3,7 +3,7 @@ class PurchaseShipping
   attr_accessor :postal_code, :prefecture_id, :city, :address, :building, :phone_number,
                 :user_id, :item_id, :token
 
-  # ここにバリデーションの処理を書く
+  # バリデーションの処理
   with_options presence: true do
     validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
     validates :city, :address, :token
@@ -12,7 +12,7 @@ class PurchaseShipping
   validates :phone_number, format: { with: /\A\d{10,11}\z/, message: 'is invalid' }
 
   def save
-    # 各テーブルにデータを保存する処理を書く
+    # データを保存する処理
     purchase_histories = PurchaseHistory.create(user_id: user_id, item_id: item_id)
 
     ShippingAddress.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city,
