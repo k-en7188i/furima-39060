5365cdce9fffd87b632328eab1eb11cd703f542e
k-en7188i <127103189+k-en7188i@users.noreply.github.com>
2023-04-14 17:02:44 +0900

    不要な記述の削除２

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,6 +1,6 @@
 const pay = () => {
   
-  const payjp = Payjp(process.env.PAYJP_PUBLIC_KEY); // PAY.JPテスト公開鍵
+  const payjp = Payjp(process.env.PAYJP_PUBLIC_KEY);
   
   const elements = payjp.elements();
   const numberElement = elements.create('cardNumber');
