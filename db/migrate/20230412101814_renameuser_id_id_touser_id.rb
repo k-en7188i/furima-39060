f27999203db4bd9c68b3e78a3eb32775b0ec81f8
k-en7188i <127103189+k-en7188i@users.noreply.github.com>
2023-04-14 18:58:20 +0900

    不要ファイルの削除１

--- db/migrate/20230412101814_renameuser_id_id_touser_id.rb
+++
@@ -1,5 +0,0 @@
-class RenameuserIdIdTouserId < ActiveRecord::Migration[6.0]
-  def change
-    rename_column :purchase_histories, :user_id_id, :user_id
-  end
-end
--- db/migrate/20230412102031_renameitem_id_id_toitem_id.rb
+++
@@ -1,5 +0,0 @@
-class RenameitemIdIdToitemId < ActiveRecord::Migration[6.0]
-  def change
-    rename_column :purchase_histories, :item_id_id, :item_id
-  end
-end
